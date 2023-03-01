221a46e1ec3c44ab925ac32e6b6fad43098733f1
ChuantaiHu <husitaizi@gmail.com>
2023-03-01 12:38:38 -0500

    Initial Commit

---
+++ Netflex Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflex Clone
+//
+//  Created by ChuantaiHu on 2023-03-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
